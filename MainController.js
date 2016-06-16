615d24d9138575808a79cd1c1ecc904b1cf87dbc
rospetbaty <petros@sourced.tech>
2016-06-16 13:22:28 +0200

    Angular
    Angularizing things

---
+++ MainController.js
@@ -0,0 +1,22 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.Me = {
+    icon: 'img/pikame.jpg',
+    title: 'ME',
+    developer: 'Professional fool',
+    price: "9999999999999999"
+  };
+
+  $scope.hobbies = {
+    icon: 'img/usd.jpg',
+    title: 'Rollerblading',
+    developer: 'Chico Dusty',
+    price: 500
+  };
+
+  $scope.result = {
+    icon: 'img/Roller.jpg',
+    title: 'frontslide',
+    developer: 'skate tricks',
+    price: 1500
+  };
+}]);
