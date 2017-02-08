dcf7ccf6bebe0300348cd84408f79e2e4c3924f3
rospetbaty <petros@sourced.tech>
2017-02-08 17:13:48 +0100

    Starting over

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  head = 'Welcome to my Website';
+  footer = 'This is just me playing around. Feel free to visit my Github: http//github.com/rospetbaty';
+}
